6f3bda3f279cad3ab70cde858ae0729e47a2faaa
Jared <jaredhernandezguadarrama@gmail.com>
2025-04-01 13:59:23 -0600

    Swagger

---
+++ FLORES/src/main/java/JaredRivera/Flores/FloresApplication.java
@@ -0,0 +1,13 @@
+package JaredRivera.Flores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresApplication.class, args);
+	}
+
+}
---
+++ FLORES/src/test/java/JaredRivera/Flores/FloresApplicationTests.java
@@ -0,0 +1,13 @@
+package JaredRivera.Flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
